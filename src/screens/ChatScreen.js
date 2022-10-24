43541e7e08b66f1a19bcb8f2ce81e38d4bc35a48
GeroWalther <gero.walther@gmail.com>
2022-10-25 00:19:20 +0200

    UI MessageComponent

---
+++ src/screens/ChatScreen.js
@@ -0,0 +1,30 @@
+import { Text, ImageBackground, StyleSheet, FlatList } from "react-native";
+
+import Message from "../components/Message/Message";
+
+import bg from "../../assets/images/BGG.jpg";
+import messages from "../../assets/data/messages.json";
+
+const ChatScreen = () => {
+  return (
+    <ImageBackground source={bg} style={styles.bg}>
+      <FlatList
+        data={messages}
+        renderItem={({ item }) => <Message message={item} />}
+        style={styles.list}
+        inverted
+      />
+    </ImageBackground>
+  );
+};
+
+export default ChatScreen;
+
+const styles = StyleSheet.create({
+  bg: {
+    flex: 1,
+  },
+  list: {
+    padding: 10,
+  },
+});
